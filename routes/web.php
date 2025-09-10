44c5964cee1077ccb72bb93f0475551d7c1f1fb7
Muhammad Alamsyah <sourcegibran@gmail.com>
2025-09-10 10:30:13 +0700

    Profile

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PermintaanController;
 use App\Http\Controllers\SuperadminController;
 use App\Http\Controllers\KepalaROController;
+use App\Http\Controllers\ProfileController;
 
 // =====================
 // DEFAULT ROUTE
@@ -94,3 +95,6 @@ Route::prefix('requestbarang')
         Route::get('/{tiket}', 'getDetail')->name('detail');
         Route::post('/', 'store')->name('store');
     });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+});
